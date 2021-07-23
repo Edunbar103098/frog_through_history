074248ee0afd4a68fc16c57822add3af1ea43260
Edunbar103098 <87675821+Edunbar103098@users.noreply.github.com>
2021-07-23 12:00:25 -0400

    Update frog_through_history.py

--- frog_through_history.py
+++ frog_through_history.py
@@ -1,1 +1,4 @@
-
+import streamlit as st
+st.sidebar.header('Sad Frog Through History')
+st.sidebar.subheader("He's just trying to survive")
+st.sidebar.image('https://i.pinimg.com/474x/63/a6/af/63a6afe6a8591720d2bef2799681c721.jpg')
